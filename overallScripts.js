4438d15aa7eade80c2ae2b7aae44e6c0717cdc70
joyrexjrl <ricklozano@gmail.com>
2023-02-07 10:20:59 -0800

    added indicator that other game is in the works
    added pic to let users know other generator is in the works

--- overallScripts.js
+++ overallScripts.js
@@ -1,9 +1,14 @@
 const osePageDisplayer = document.getElementById("ose_sheet_display");
 const oseLogoButton = document.getElementById("ose_logo_button");
 const welcomeScreen = document.getElementById("welcome_screen");
+const underConstruction = document.getElementById("under_construction");
 
 function openOsePage(){
     welcomeScreen.classList.toggle("hide");
     osePageDisplayer.classList.toggle("hide");
     oseLogoButton.classList.toggle("logo_outline");
+}
+
+function openShadowrunPage(){
+    
 }
