1a2bbfc7e2b51f0a88e0eeed05f9d6aa4fe0514d
joyrexjrl <ricklozano@gmail.com>
2023-02-07 09:56:18 -0800

    outline added to icon buttons to clearly display which sheet is active
    added outline to the game logo when clicked

--- overallScripts.js
+++ overallScripts.js
@@ -1,5 +1,7 @@
 const osePageDisplayer = document.getElementById("ose_sheet_display");
+const oseLogoButton = document.getElementById("ose_logo_button");
 
 function openOsePage(){
     osePageDisplayer.classList.toggle("hide");
+    oseLogoButton.classList.toggle("logo_outline");
 }
