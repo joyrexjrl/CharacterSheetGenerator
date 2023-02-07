0550613887ef0a555573d41767f15eabae0dc3c0
joyrexjrl <ricklozano@gmail.com>
2023-02-07 10:04:42 -0800

    welcome screen added
    rough welcome screen added, simple code added to hide screen when ose generator is opened

--- overallScripts.js
+++ overallScripts.js
@@ -1,7 +1,9 @@
 const osePageDisplayer = document.getElementById("ose_sheet_display");
 const oseLogoButton = document.getElementById("ose_logo_button");
+const welcomeScreen = document.getElementById("welcome_screen");
 
 function openOsePage(){
+    welcomeScreen.classList.toggle("hide");
     osePageDisplayer.classList.toggle("hide");
     oseLogoButton.classList.toggle("logo_outline");
 }
