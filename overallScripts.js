6420d3639bf3abfaccb4d1860e2032656dc5254b
joyrexjrl <ricklozano@gmail.com>
2023-02-07 23:07:11 -0800

    added button functionality
    linked the buttons in the header to the corresponding sections to show and hide when clicked. if no generator is selected then the welcome page will come back up.

--- overallScripts.js
+++ overallScripts.js
@@ -1,14 +1,24 @@
-const osePageDisplayer = document.getElementById("ose_sheet_display");
-const oseLogoButton = document.getElementById("ose_logo_button");
 const welcomeScreen = document.getElementById("welcome_screen");
-const underConstruction = document.getElementById("under_construction");
+const gameGeneratorIcons = document.querySelectorAll(".game_sheet_buttons");
+const pageSections = document.querySelectorAll(".page_dislayer");
 
-function openOsePage(){
-    welcomeScreen.classList.toggle("hide");
-    osePageDisplayer.classList.toggle("hide");
-    oseLogoButton.classList.toggle("logo_outline");
-}
+gameGeneratorIcons.forEach(button => {
+    button.addEventListener("click", () => {
+        const buttonId = button.getAttribute("id");
+        const selectionId = buttonId.replace("logo_button", "sheet_display");
+        const section = document.getElementById(selectionId);
 
-function openShadowrunPage(){
-    
-}
+        if(section.classList.contains("hide")){
+            gameGeneratorIcons.forEach(btn => btn.classList.remove("logo_outline"));
+            button.classList.add("logo_outline");
+
+            pageSections.forEach(sect => sect.classList.add("hide"));
+            section.classList.remove("hide");
+            welcomeScreen.classList.add("hide");
+        }else{
+            button.classList.remove("logo_outline");
+            section.classList.add("hide");
+            welcomeScreen.classList.remove("hide");
+        }
+    });
+});
