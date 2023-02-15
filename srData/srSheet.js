cc649ed4b198f145361c1771d351f117dd3bf110
joyrexjrl <ricklozano@gmail.com>
2023-02-15 01:23:01 -0800

    more layout for shadowrun added
    header section and attributes section layout in place on the sheet. some js added to space out the attributes when added to page, currently sitting at 0 for markup.

--- srData/srSheet.js
+++ srData/srSheet.js
@@ -1,5 +1,17 @@
 const srAttributeBbody = document.getElementById("sr_attribute_body");
 const srAttributeQuickness = document.getElementById("sr_attribute_quickness");
+const srAttributeStrength = document.getElementById("sr_attribute_strength");
+const srAttributeCharisma = document.getElementById("sr_attribute_charisma");
+const srAttributeIntelligence = document.getElementById("sr_attribute_intelligence");
+const srAttributeWillpower = document.getElementById("sr_attribute_willpower");
+const srAttributeEssence = document.getElementById("sr_attribute_essence");
+const srAttributeMagic = document.getElementById("sr_attribute_magic");
 
 srAttributeBbody.innerHTML = 0;
 srAttributeQuickness.innerHTML = 0;
+srAttributeStrength.innerHTML = 0;
+srAttributeCharisma.innerHTML = 0;
+srAttributeIntelligence.innerHTML = 0;
+srAttributeWillpower.innerHTML = 0;
+srAttributeEssence.innerHTML = 0;
+srAttributeMagic.innerHTML = 0;
