51034985dac6edd5b4704756e6170b3021c00793
joyrexjrl <ricklozano@gmail.com>
2023-02-14 20:25:08 -0800

    started layout for shadowrun
    downloaded and installed font and strted layout for shadowrun sheet

---
+++ srData/srSheet.js
@@ -0,0 +1,5 @@
+const srAttributeBbody = document.getElementById("sr_attribute_body");
+const srAttributeQuickness = document.getElementById("sr_attribute_quickness");
+
+srAttributeBbody.innerHTML = 0;
+srAttributeQuickness.innerHTML = 0;
