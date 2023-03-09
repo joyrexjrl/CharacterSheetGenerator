40769d69789240d34be7e6f893ddacc40504b4e8
joyrexjrl <ricklozano@gmail.com>
2023-03-09 10:34:24 -0800

    refactored weapons code
    changing weapons code to make more sense to me when info is being passed into the weapon creation function.

---
+++ srData/srWeaponFunctions.js
@@ -0,0 +1,192 @@
+var srWeaponCost = 0;
+
+function srWeaponRangeFinder(weaponType){
+    let short = "";
+    let medium = "";
+    let long = "";
+    let extreme = "";
+    switch (weaponType) {
+        case "Hold-out Pistol":
+        case "Light Pistol":
+            short = "0-5";
+            medium = "6-15";
+            long = "16-30";
+            extreme = "31-50";
+        break;
+        case "Heavy Pistol":
+            short = "0-5";
+            medium = "6-20";
+            long = "21-40";
+            extreme = "41-60";
+        break;
+        case "SMG":
+            short = "0-10";
+            medium = "11-40";
+            long = "41-80";
+            extreme = "81-150";
+        break;
+        case "Taser":
+            short = "0-5";
+            medium = "6-10";
+            long = "11-12";
+            extreme = "13-15";
+        break;
+        case "Shotgun":
+            short = "0-10";
+            medium = "11-20";
+            long = "21-50";
+            extreme = "51-100";
+        break;
+        case "Sporting Rifle":
+            short = "0-100";
+            medium = "101-250";
+            long = "251-500";
+            extreme = "501-750";
+        break;
+        case "Sniper Rifle":
+            short = "0-150";
+            medium = "151-300";
+            long = "301-700";
+            extreme = "701-1000";
+        break;
+        case "Assault Rifle":
+            short = "0-50";
+            medium = "51-150";
+            long = "151-350";
+            extreme = "351-550";
+        break;
+        case "Light Machine Guns":
+            short = "0-75";
+            medium = "76-200";
+            long = "201-400";
+            extreme = "401-800";
+        break;
+        case "Medium Machine Guns":
+            short = "0-80";
+            medium = "81-250";
+            long = "251-750";
+            extreme = "751-1200";
+        break;
+        case "Heavy Machine Guns":
+            short = "0-80";
+            medium = "81-250";
+            long = "251-800";
+            extreme = "801-1500";
+        break;
+        case "Assault Cannon":
+            short = "0-100";
+            medium = "101-300";
+            long = "301-900";
+            extreme = "901-2400";
+        break;
+        case "Grenade Launcher":
+            short = "5-50";
+            medium = "51-100";
+            long = "101-150";
+            extreme = "151-300";
+        break;
+        case "Missile Launcher":
+            short = "20-150";
+            medium = "151-450";
+            long = "451-1200";
+            extreme = "1201-3000";
+        break;
+        case "Bow":
+            short = "0-STR";
+            medium = "To STRx10";
+            long = "To STRx30";
+            extreme = "To STRx60";
+        break;
+        case "Light Crossbow":
+            short = "0-STRx2";
+            medium = "To STRx8";
+            long = "To STRx20";
+            extreme = "To STRx40";
+        break;
+        case "Medium Crossbow":
+            short = "0-STRx3";
+            medium = "To STRx12";
+            long = "To STRx30";
+            extreme = "To STRx50";
+        break;
+        case "Heavy Crossbow":
+            short = "0-STRx5";
+            medium = "To STRx15";
+            long = "To STRx40";
+            extreme = "To STRx60";
+        break;
+        case "Thrown Knife":
+            short = "0-STR";
+            medium = "To STRx2";
+            long = "To STRx3";
+            extreme = "To STRx5";
+        break;
+        case "Shuriken":
+            short = "0-STR";
+            medium = "To STRx2";
+            long = "To STRx5";
+            extreme = "To STRx7";
+        break;
+    }
+
+    return {short: short, medium: medium, long: long, extreme: extreme};
+}
+
+function srWeaponBuilder(weaponType, weaponName){
+    const weaponRange = srWeaponRangeFinder(weaponType);
+
+    let srWeaponName = weaponName;
+    let srWeaponType = "";
+    let srWeaponConceal = "";
+    let srWeaponReach = "";
+    let srWeaponMode = "";
+    let srWeaponAmmo = "";
+    let srShort = weaponRange.short;
+    let srMedium = weaponRange.medium;
+    let srLong = weaponRange.long;
+    let srExtreme = weaponRange.extreme;
+    let srWeaponDamage = "";
+    let srWeaponMods = "";
+    
+    let bowStrMin = 0;
+
+    switch(weaponName){
+        case "Bow":
+            bowStrMin = oseDieRoller(1, 4);
+            srWeaponConceal = "2";
+            srWeaponReach = "-";
+            srWeaponMode = "-";
+            srWeaponAmmo = "-";
+            srWeaponDamage = "(STR " + bowStrMin+2 + ")M";
+            srWeaponMods = "STR Min " + bowStrMin+1;
+            srWeaponCost = bowStrMin*100; 
+        break;
+        case "Ranger-X Bow":
+            bowStrMin = oseDieRoller(1, 4);
+            srWeaponConceal = "2";
+            srWeaponReach = "-";
+            srWeaponMode = "-";
+            srWeaponAmmo = "-";
+            srWeaponDamage = "(STR " + bowStrMin+4 + ")M";
+            srWeaponMods = "STR Min " + bowStrMin+2;
+            srWeaponCost = bowStrMin*120; 
+        break;
+        case "Light Crossbow":
+        break;
+        case "Medium Crossbow":
+        break;
+        case "Heavy Crossbow":
+        break;
+        case "Pistol Crossbow":
+        break;
+        case "Repeater Crossbow":
+        break;
+        case "Throwing Knife":
+        break;
+        case "Shuriken":
+        break;
+    } 
+
+    return {name: srWeaponName, type: srWeaponType, conceal: srWeaponConceal, reach: srWeaponReach, mode: srWeaponMode, ammo: srWeaponAmmo,
+        rangeShort: srShort, rangeMedium: srMedium, rangeLong: srLong, rangeExtreme: srExtreme, damage: srWeaponDamage, modifiers: srWeaponMods};
+}
--- srData/srWeapons.js
+++ srData/srWeapons.js
@@ -1,69 +1,27 @@
 const srMeleeWeapons = [
-    {weaponName: "Forearm Snap Blades", reach: 0, damage: "(STR)M", cost: 850},
-    {weaponName: "Katana", reach: 1, damage: "(STR+3)M", cost: 1000},
-    {weaponName: "Knife", reach: 0, damage: "(STR)L", cost: 30},
-    {weaponName: "Sword", reach: 1, damage: "(STR+2)M", cost: 500},
-    {weaponName: "Survival Knife", reach: 0, damage: "(STR+2)L", cost: 450},
-    {weaponName: "Club", reach: 1, damage: "(STR+1)M Stun", cost: 10},
-    {weaponName: "Sap", reach: 0, damage: "(STR+2)M Stun", cost: 10},
-    {weaponName: "Stun Baton", reach: 1, damage: "6S Stun", cost: 750},
-    {weaponName: "Combat Axe", reach: 2, damage: "(STR)S", cost: 750},
-    {weaponName: "Pole Arm", reach: 2, damage: "(STR+3)S", cost: 500},
-    {weaponName: "Staff", reach: 2, damage: "(STR+2)M Stun", cost: 50},
-    {weaponName: "Monofilament Whip", reach: 2, damage: "10S", cost: 3000},
-    {weaponName: "Shock Glove", reach: 0, damage: "(STR-1)M Stin + 7S Stun", cost: 950},
-    {weaponName: "Whip", reach: 2, damage: "(STR)L", cost: 300},
-    {weaponName: "Cougar Fineclade Knife (short)", reach: 0, damage: "(STR)M", cost: 800},
-    {weaponName: "Cougar Fineclade Knife (Long)", reach: 0, damage: "(STR+1)M", cost: 1500},
-    {weaponName: "Katar", reach: 0, damage: "(STR+3)L", cost: 800},
-    {weaponName: "Kris", reach: 0, damage: "(STR)M", cost: 1000},
-    {weaponName: "Laser Crescent Axe", reach: 1, damage: "(STR)S", cost: 3500},
-    {weaponName: "Macauitl", reach: 1, damage: "(STR+2)M", cost: 3000},
-    {weaponName: "Ares Monosword", reach: 1, damage: "(STR+3)M", cost: 1000},
-    {weaponName: "Tomahawk", reach: 1, damage: "(STR)M", cost: 200},
-    {weaponName: "Vibro Knife", reach: 0, damage: "(STR)M", cost: 1000},
-    {weaponName: "Vibro Sword", reach: 1, damage: "(STR)S", cost: 2000},
-    {weaponName: "Extendable Baton", reach: 1, damage: "(STR+1)M Stun", cost: 25},
-    {weaponName: "Mace", reach: 1, damage: "(STR+3)M Stun", cost: 150},
-    {weaponName: "Spiked Mace", reach: 1, damage: "(STR+1)M", cost: 150},
-    {weaponName: "Rattan Stick", reach: 1, damage: "(STR+1)M Stun", cost: 10},
-    {weaponName: "Sai", reach: 0, damage: "(STR+2)L Stun", cost: 100},
-    {weaponName: "AZ - 150 Stun Baton", reach: 1, damage: "8S Stun", cost: 1500},
-    {weaponName: "Bo Staff", reach: 2, damage: "(STR+3 M Stun)", cost: 50},
-    {weaponName: "Claymore", reach: 2, damage: "(STR+1)S", cost: 1000},
-    {weaponName: "Harpoon", reach: 2, damage: "(STR+2)M", cost: 500},
-    {weaponName: "Javelin", reach: 2, damage: "(STR+2)L", cost: 100},
-    {weaponName: "No-Dachi", reach: 2, damage: "(STR+1)S", cost: 3000},
-    {weaponName: "Spear", reach: 2, damage: "(STR+4)L", cost: 200},
-    {weaponName: "Telescoping Staff", reach: 2, damage: "(STR+2)M Stun", cost: 100},
-    {weaponName: "Kusarigama", reach: 2, damage: "(STR+1)M", cost: 200},
-    {weaponName: "Manriki-gusari", reach: 2, damage: "(STR+2)L Stun", cost: 150},
-    {weaponName: "Morning Star", reach: 2, damage: "(STR+2)M", cost: 250},
-    {weaponName: "Nunchaku", reach: 1, damage: "(STR+1)M Stun", cost: 100},
-    {weaponName: "Three-section Staff", reach: 2, damage: "(STR+2)M Stun", cost: 200}
+    {type: "Personal Weapons", name: ["Forearm Snap Blades", "Katana", "Knife", "Sword", "Survival Knife", "Club", "Sap", "Stun Baton", "Combat Axe", "Pole Arm", "Staff", "Monofilament Whip", "Shock Glove", "Whip", "Cougar Fineclade Knife (short)", "Cougar Fineclade Knife (Long)", "Katar", "Kris", "Laser Crescent Axe", "Macauitl", "Ares Monosword", "Tomahawk", "Vibro Knife", "Vibro Sword", "Extendable Baton", "Mace", "Spiked Mace", "Rattan Stick", "Sai", "AZ - 150 Stun Baton", "Bo Staff", "Claymore", "Harpoon", "Javelin", "No-Dachi", "Spear", "Telescoping Staff", "Kusarigama", "Manriki-gusari", "Morning Star", "Nunchaku", "Three-section Staff"]}
 ];
 
 const srProjectileWeapons = [
-    {weaponName: "Bow", minSTR: 0, damage: "", cost: 0, newObject: function(){return customFunctionHere()}},
-    {weaponName: "Crossbow", minSTR: 0, damage: "", cost: 0, newObject: function(){return crossbowCode()}},
-    {weaponName: "Throwing Knife", minSTR: 0, damage: "(STR)L", cost: 20},
-    {weaponName: "Shuriken", minSTR: 0, damage: "(STR)L", cost: 30}
+    {type: "Standard Bow", name: []},
+    {type: "Crossbow", name: []},
+    {type: "Throwing Weapons", name: []}
 ];
 
 const srFirearmWeapons = [
-    {weaponName: "Hold-Out", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Light", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Machine Pistols", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Heavy Pistols", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Special Weapons", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "SMGs", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Rifles", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Shotguns", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Assault Rifles", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Machine Guns", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Heavy Weapons", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Grenade Launchers", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}},
-    {weaponName: "Missile Launchers", ammo: "", mode: "", damage: "", cost: 0, newObject: function(){return allTheFunctionsHere()}}
+    {type: "Hold-Out", name: []},
+    {type: "Light", name: []},
+    {type: "Machine Pistols", name: []},
+    {type: "Heavy Pistols", name: []},
+    {type: "Special Weapons", name: []},
+    {type: "SMGs", name: []},
+    {type: "Rifles", name: []},
+    {type: "Shotguns", name: []},
+    {type: "Assault Rifles", name: []},
+    {type: "Machine Guns", name: []},
+    {type: "Heavy Weapons", name: []},
+    {type: "Grenade Launchers", name: []},
+    {type: "Missile Launchers", name: []}
 ];
 
 const srWeaponsToChooseFromArray = [srMeleeWeapons, srProjectileWeapons, srFirearmWeapons];
@@ -87,7 +45,7 @@ function srWeaponsPicker(){
     let totalWeaponCost = 0;
     
     let weaponType = srWeaponsToChooseFromArray[Math.floor(Math.random() * srWeaponsToChooseFromArray.length)];    
-    let chosenWeapon = weaponType[Math.floor(Math.random() * weaponType.length)].weaponName;
+    let chosenWeapon = weaponType[Math.floor(Math.random() * weaponType.length)].type;
 
     if(weaponType == srMeleeWeapons){
         weaponRangeShort = "-";
@@ -107,8 +65,10 @@ function srWeaponsPicker(){
         srResourceAmount -= totalWeaponCost;
         //create HTML placer here
         srWeaponInfoClearAll()
+        srWeaponCost = 0;
     }else{
         srWeaponInfoClearAll()
+        srWeaponCost = 0;
         return;
     }        
 }
